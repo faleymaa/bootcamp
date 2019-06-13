91cc775f9078caeb42adfbf217977cd1fab84923
Megan Faley <megan.faley@gmail.com>
2019-06-13 23:41:14 +0000

    font update

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,6 @@ module Splurty
     config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf)
+    config.assets.precompile += %w( .woff2 .woff .ttf)
   end
 end
